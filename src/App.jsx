5e6a2e53c3d4217cece4fe2a1d54aa58a64e84dc
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-08-29 01:19:30 +0200

    [UPD] buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
-function App() {}
+import Header from "./components/Header";
+import Button from "./components/Button";
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Button />
+      </div>
+    </>
+  );
+}
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="title-container">
+      <h1 className="p-3">Learn Web development</h1>
+    </div>
+  );
+};
+
+export default Header;
